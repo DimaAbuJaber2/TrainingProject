e851c23b611094e9a80721cd15395cbf937f4149
Dima-Abu-Jaber422 <dimaabujaber422000@gmail.com>
2022-12-18 19:10:00 +0200

    first commit

---
+++ src/test/java/com/example/cityHotel/CityHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cityHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
